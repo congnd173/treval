7eeba56be8a886cc0e5548877db9e8d9395a0b4e
congprodh <congprodh@gmail.com>
2023-05-10 04:54:57 +0700

    first commit

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,11 @@
+'use item';
+
+import Image from "next/image"
+
+const Avatar = () => {
+    return (
+        <Image alt="Avatar" src="/images/placeholder.jpg" className="rounded-full" height="30" width="30" />
+    )
+}
+
+export default Avatar
