6ab2c0049d9551381f2b28589d68d7000413490d
congnd173 <ndcong1703@gmail.com>
2023-09-24 22:07:55 +0700

    feat: reservation screen

--- app/reservations/ReservationClient.tsx
+++ app/reservations/ReservationClient.tsx
@@ -1,36 +1,40 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { toast } from "react-hot-toast";
+import axios from "axios";
 import { useCallback, useState } from "react";
-
-import Container from "../components/Container";
-import Heading from "../components/Heading";
+import { useRouter } from "next/navigation";
 
 import { SafeReservation, SafeUser } from "../types";
-import axios from "axios";
-import toast from "react-hot-toast";
+
+import Heading from "../components/Heading";
+import Container from "../components/Container";
 import ListingCard from "../components/listings/ListingCard";
 
-interface TripClientProps {
+interface ReservationClientProps {
   reservations: SafeReservation[];
-  currentUser: SafeUser | null;
+  currentUser?: SafeUser | null;
 }
 
-const TripClient = ({ reservations, currentUser }: TripClientProps) => {
+const ReservationClient = ({
+  reservations,
+  currentUser,
+}: ReservationClientProps) => {
   const router = useRouter();
   const [deletingId, setDeletingId] = useState("");
 
   const onCancel = useCallback(
     (id: string) => {
       setDeletingId(id);
+
       axios
         .delete(`/api/reservations/${id}`)
         .then(() => {
-          toast.success("Reservation cancelled!");
+          toast.success("Reservation cancelled");
           router.refresh();
         })
-        .catch((error) => {
-          toast.error(error?.response?.data?.error);
+        .catch(() => {
+          toast.error("Something went wrong");
         })
         .finally(() => {
           setDeletingId("");
@@ -40,11 +44,8 @@ const TripClient = ({ reservations, currentUser }: TripClientProps) => {
   );
   return (
     <Container>
-      <Heading
-        title="Your trips"
-        subtitle="Where you've been and where you're going"
-      />
-      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+      <Heading title="Reservations" subtitle="Bookings on your properties" />
+      <div className=" mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {reservations.map((reservation) => (
           <ListingCard
             key={reservation.id}
@@ -53,8 +54,7 @@ const TripClient = ({ reservations, currentUser }: TripClientProps) => {
             actionId={reservation.id}
             onAction={onCancel}
             disabled={deletingId === reservation.id}
-            actionLabel="Cancel reservation"
-            currentUser={currentUser}
+            actionLabel="Deny reservation"
           />
         ))}
       </div>
@@ -62,4 +62,4 @@ const TripClient = ({ reservations, currentUser }: TripClientProps) => {
   );
 };
 
-export default TripClient;
+export default ReservationClient;
---
+++ app/reservations/page.tsx
@@ -0,0 +1,43 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+import ReservationClient from "./ReservationClient";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+
+const ReservationPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subtitle="Please login" />
+      </ClientOnly>
+    );
+  }
+
+  const reservations = await getReservations({
+    authorId: currentUser.id,
+  });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="You had no reservations yet"
+          subtitle="Looks like there is no one book your place yet"
+        />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <ReservationClient
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  );
+};
+
+export default ReservationPage;
